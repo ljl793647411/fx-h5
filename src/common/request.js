0e6f7ca03c1de211566beb4e4e17ce235058b6d8
ljl793647411 <793647411@qq.com>
2022-03-11 19:47:42 +0800

    fix: 请求参数修改

--- src/common/request.js
+++ src/common/request.js
@@ -35,7 +35,7 @@ module.exports = (vm) => {
 	// 请求拦截
 	uni.$u.http.interceptors.request.use((config) => { // 可使用async await 做异步操作
 	    // 初始化请求拦截器时，会执行此方法，此时data为undefined，赋予默认{}
-	    config.data = config.data || {}
+	    config.data = config.data
 		// 根据custom参数中配置的添加对应的Authorization
 		const auth = config.custom.auth
 		if (auth) {
