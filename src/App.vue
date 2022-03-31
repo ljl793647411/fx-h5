a7e1585bf623de58aae9577a9910e1db6dd51c5e
ljl793647411 <793647411@qq.com>
2022-04-01 01:31:53 +0800

    fix: 删除无用代码

--- src/App.vue
+++ src/App.vue
@@ -4,28 +4,6 @@
         onLaunch: async function() {
 		},
 		onShow: function() {
-            // dd.ready(() => {
-            //     dd.runtime.permission.requestAuthCode({
-            //         corpId: 'ding6f768bca630f8220',
-            //         onSuccess: result => {
-            //             const getUser = require('@/common/http.api.js').getUser
-            //             // 调用免登
-            //             getUser(result.code).then(() => {
-            //                 // 授权认证成功
-            //                 window._isAuth = true
-            //             }).catch(e => {
-            //                 uni.$u.toast(e.message || JSON.stringify(e));
-            //             });
-            //         },
-            //         onFail: err => {
-            //             // window.ddErr = err;
-            //             uni.$u.toast(err.message || JSON.stringify(err));
-            //         }
-            //     });
-            // });
-            // dd.error(error => {
-            //     uni.$u.toast('dd error----: ' + JSON.stringify(error));
-            // });
 		},
 		onHide: function() {
 		}
